a9affd5b17bd05e5f6dff697ff3d83b55e075281
M8SZT8 <h0tak88r@gmail.com>
2023-08-17 02:27:37 +0300

    Adding 2FA Security Testing guide

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230612.113446';
+const cacheName = 'chirpy-20230817.022538';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
